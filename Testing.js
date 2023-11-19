a1eca424e81b7db6f78b9eb090a8dd5b106d61a9
JanaEl-khatib <jel-khat@my.loyno.edu>
2023-11-18 21:47:44 -0600

    API WORKED :D

--- Testing.js
+++
@@ -1,7 +0,0 @@
-
-
-console.log('hello1');
-
-function a1 () {
-    console.log ("I am in a1 function");
-}
--- index.js
+++
@@ -1,9 +0,0 @@
-const araby = require("./src/araby");
-const number = require("./src/number");
-const trans = require("./src/trans/trans");
-
-module.exports = {
-  ...araby,
-  ...number,
-  ...trans,
-};
